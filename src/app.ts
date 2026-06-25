eafc26b97d7703d6180c8daf4ebf3b8693b7cf69
fidel1408 <fidel.rmt66@gmail.com>
2026-06-25 00:18:08 -0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(
 );
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ extended: true, limit: "25mb" }));
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
 app.get("/", (_req, res) => {
   res.send("Fluent English Renderer is running.");
